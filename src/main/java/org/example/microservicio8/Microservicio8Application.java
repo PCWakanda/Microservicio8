73748d7990e113cfd09cad07e96f9727a625da56
jmartter <150058251+jmartter@users.noreply.github.com>
2024-12-01 17:13:45 +0100

    Initial commit

---
+++ src/main/java/org/example/microservicio8/Microservicio8Application.java
@@ -0,0 +1,13 @@
+package org.example.microservicio8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservicio8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservicio8Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/microservicio8/Microservicio8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microservicio8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicio8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
